46786c0ab81daff2c5ba5c17975f38090fb3a5d1
mauriciomhv2013-ship-it <mauriciomhv2013@gmail.com>
2026-09-22 18:36:49 -0300

    Reactivar recordatorios al encender Android

---
+++ neon-control/app/src/main/java/com/sol/neoncontrol/BootReceiver.java
@@ -0,0 +1,11 @@
+package com.sol.neoncontrol;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        ReminderReceiver.schedule(context);
+    }
+}
